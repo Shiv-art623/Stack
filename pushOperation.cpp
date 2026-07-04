dee2d60dbf34a4a0f6df4f91bbfc0082ad719252
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-07-04 23:39:45 +0530

    Push Operation using array implementation

---
+++ pushOperation.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    public:
+    int *arr;
+    int size;
+    int top;
+Stack(int s){
+    size = s;
+    top= -1;
+    arr = new int [s];
+}
+
+void push(int value){
+    if(top == size-1) {cout<<"stack overflow"<<endl;
+    return;}
+else{
+    top++;
+    arr[top] = value;
+}
+}
+};
+int main() {
+    Stack st(5);   // Stack of size 5
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    // This will cause overflow
+    st.push(60);
+
+    // Printing stack elements
+    cout << "Stack elements: ";
+    for(int i = 0; i <= st.top; i++){
+        cout << st.arr[i] << " ";
+    }
+
+    return 0;
+}
