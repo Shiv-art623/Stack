dab074b8352607d80f25fd3c40441a3210226fb3
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-07-06 00:41:07 +0530

    Implemented using LL

---
+++ StackUsingLinkedList.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+int data;
+Node* next;
+
+Node(int value){
+    data = value;
+    next = NULL;
+}
+};
+
+class Stack{
+    Node *top;
+    int size;
+
+    public:
+    Stack(){
+        top = NULL;
+        size = 0;
+    }
+
+    void push(int value){
+        Node* temp = new Node(value);
+        if(temp == NULL){
+            cout<<"stack Overflow"<<endl;
+            return;
+        }
+        else{
+            temp->next = top;
+            top = temp;
+            size++;
+        }
+    }
+
+    void pop(){
+        if(top == NULL){
+            cout<<"stack underflow"<<endl;
+            return;
+        }
+        else{
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+            size--;
+        }
+    }
+
+    int peek(){
+        if(top == NULL){
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+        else return top->data;
+    }
+};
+int main(){
+    Stack s;
+    s.push(2);
+    cout<<s.peek();
+    s.pop();
+    cout<<s.peek();
+    return 0;
+}
