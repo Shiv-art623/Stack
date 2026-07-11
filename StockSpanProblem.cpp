2c816d4f3ef1ef82003caae3f90ad96bfc15d0c4
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-07-12 01:55:07 +0530

    Stock span problem

---
+++ StockSpanProblem.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> stockSpan(int arr[], int n){
+    stack<int> st;
+    vector<int> ans(n,1);
+    for(int i = n-1; i>=0; i--){
+        while(!st.empty() && arr[i]> arr[st.top()]){
+            ans[st.top()] = st.top()-i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        ans[st.top()] = st.top()+1;
+        st.pop();
+    }
+    return ans;
+}
+int main(){
+    int arr[] = {100,80,55,70,60,75,85};
+    vector<int>ans = stockSpan(arr,7);
+    for(int i =0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
