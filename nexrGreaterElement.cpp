dd9451e0c83d5bb7eb0c3992d24d0b30ab19cc3c
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-07-11 23:55:28 +0530

    Optimal

--- nexrGreaterElement.cpp
+++ nexrGreaterElement.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//Brute force
 vector<int> nextGreater(int arr[], int n){
      vector<int> ans(n,-1);
      for(int i =0; i<n; i++){
@@ -12,13 +12,27 @@ vector<int> nextGreater(int arr[], int n){
         }
      }
      return ans;
-}
+}//TC = O(n^2)
+
+
+//Optimal
+vector<int> nextGreater2(int arr[], int n){
+    stack<int> st;
+    vector<int> ans(n,-1);
+    for(int i =0; i<n; i++){
+        while(!st.empty() && arr[st.top()] < arr[i]){
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans;
+}//TC = O(n) and SC = O(n)
 int main(){
     int arr[] = {8,6,4,7,4,9,10,8,12};
-    vector<int> ans = nextGreater(arr,9);
+    vector<int> ans = nextGreater2(arr,9);
     for(int i =0; i<ans.size(); i++){
         cout<<ans[i]<<" ";
     }
     return 0;
 }
-//TC = O(n^2)
