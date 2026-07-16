0168f02ee477b38c7703c241b721531a4f8aab41
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-07-17 01:01:46 +0530

    second way is added

--- LargestRectangleINHistogram.cpp
+++ LargestRectangleINHistogram.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//three times of traversal or array needed
 int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         stack<int> st;
@@ -34,8 +34,35 @@ int largestRectangleArea(vector<int>& heights) {
         }
         return ans;
     }
+
+    //with single pass----->
+     int largestRectangleArea2(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int ans =0;
+        for(int i =0; i<n; i++){
+            while(!st.empty() && heights[st.top()]>heights[i] ){
+                int index = st.top();
+                st.pop();
+
+                if(!st.empty()) ans = max(ans, heights[index]*(i-st.top()-1));
+                else ans = max(ans,heights[index]*i);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int index = st.top();
+            st.pop();
+            if(!st.empty()) ans = max(ans, heights[index]*(n- st.top()-1));
+            else ans = max(ans, heights[index]*n);
+        }
+        return ans;
+    }
     int main(){
         vector<int> arr = {2,3,4,2,6,5,4,5,3};
-        cout<<largestRectangleArea(arr);
+        cout<<largestRectangleArea(arr)<<endl;
+        cout<<largestRectangleArea2(arr);
+        return 0;
     }
     //TC = O(n) and SC = O(n)
